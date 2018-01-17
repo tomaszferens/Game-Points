a67f608f3e8d1f20bd938c19096ea2359844acba
Tomasz Ferens <tomaszferens@hotmail.com>
2018-01-17 14:25:34 +0100

    get rid of commented code

--- src/ItemsBoard/reducers/cartReducer.ts
+++ src/ItemsBoard/reducers/cartReducer.ts
@@ -3,8 +3,6 @@ import { NEW_GAME } from 'src/ItemsBoard/actions/actions'
 import { ADD_TO_CART } from 'src/ItemsList/actions/actions'
 import { CartState } from 'src/models/Item'
 
-// import { omit as _omit } from 'lodash'
-
 export const cart = (state: CartState = {}, action: AppAction) => {
     switch (action.type) {
         case ADD_TO_CART:
@@ -12,16 +10,6 @@ export const cart = (state: CartState = {}, action: AppAction) => {
             return { ...state, [id]: state[id] ? state[id] + 1 : 1 }
         case NEW_GAME:
             return {}
-        // case REMOVE_FROM_CART:
-        //     const removeFromId = action.payload.id
-        //     return state[removeFromId] && state[removeFromId] > 1
-        //         ? { ...state, [removeFromId]: state[removeFromId] - 1 }
-        //         : _omit(state, removeFromId)
-        // case SET_PRODUCT_AMOUNT:
-        //     const updateId = action.payload.id
-        //     return action.payload.amount > 0
-        //         ? { ...state, [updateId]: Number(action.payload.amount) }
-        //         : _omit(state, updateId)
         default:
             return state
     }
--- src/helpers/helper.ts
+++
@@ -1,9 +0,0 @@
-import { range as _range } from 'lodash'
-
-const asciiCharA = 65
-export const items = _range(26).reduce((acc, val, i) => {
-    return {
-        ...acc,
-        [`id${i}`]: { name: String.fromCharCode(asciiCharA + val), price: Math.random() },
-    }
-}, {})
